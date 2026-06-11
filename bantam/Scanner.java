811d8e41e596ed79d44c746a987660526c829a4c
delightaheebwa <aheebwadelight@gmail.com>
2026-06-11 23:25:05 +0300

    string, number literals & identifiers

--- bantam/Scanner.java
+++ bantam/Scanner.java
@@ -13,6 +13,28 @@ class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);    
+        keywords.put("if", IF);   
+        keywords.put("nil", NIL); 
+        keywords.put("or", OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while",  WHILE);
+    }
+
     private int start = 0;
     private int current = 0;
     private int line = 1;
@@ -77,15 +99,71 @@ class Scanner {
             // we wanted to handle incrementing the line counter when faced with a new line
             line++;
             break;
+        case '"': string(); break;
         
         // gives individual errors but a "combined error report" would be better
         // imagine fixing one error then the next springs up just after
         // Since hadError gets set, we’ll never try to execute any of the code, even though we keep going and scan the rest of it
         default:
-            Bantam.error(line, "Unexpected character");
+            if (isDigit(c)) {
+                number();
+            // we begin by assuming any lexeme starting with a letter or underscore is an identifier otherwise it will be a keyword
+            // if when the identifier is found in the lookup. Remember a reserved word/keyword IS also an identifier
+            // also remember they should start with an alphabetical letter hence isAlpha() first and then under identifier we account 
+            // for the numeric till the end of the identifier
+            } else if (isAlpha()) {
+                identifier();
+            } else {
+                Bantam.error(line, "Unexpected character");
+            }
             break;
      }     
   }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while ((isDigit(peek()))) advance();
+
+        // look for a fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            // consume the "." now that we are assured a digit follows after it
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+        // convert the lexeme to its numeric value
+        // interpreter uses Java’s Double type to represent numbers, so we produce a value of that type.
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));;
+    }
+
+    private void string() {
+        // X: what about single quotes?
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        
+        if (isAtEnd()) {
+            Bantam.error(line, "Unterminated string");
+            return;
+        }
+        
+        // the closing "
+        advance();
+
+        // trim the surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
 
     private boolean match(char expected) {
         if(isAtEnd()) return false;
@@ -101,6 +179,24 @@ class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 > source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+           (c>= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {
@@ -125,8 +221,6 @@ class Scanner {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
-
-
 
 
 }
